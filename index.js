4acdd35e9a0f2d955b430520895ddbe85c8b73d1
phiny-steph <86403160+phiny-steph@users.noreply.github.com>
2021-11-07 15:31:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -124,6 +124,29 @@ rainbow.splice(5,0, "Purple");
 console.log(rainbow);
 
 
+/*
+5. In the Harry Potter novels, children attending the Hogwarts School of Witchcraft and Wizardry belong to one of four houses: Gryffindor, Hufflepuff, Ravenclaw, or Slytherin. Each year, the houses assemble a Quidditch team of seven players to compete for the coveted Quidditch Cup.
+Consider the following array.
+
+const team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
+Create a function called hasEnoughPlayers() that takes the team array as an argument and returns true or false depending on if the array has at least seven players.
+/*
+* Programming Quiz: Quidditch Cup
+*/
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a function `hasEnoughPlayers()`
+* - Your function `hasEnoughPlayers()` should accept one parameter
+* - Your function `hasEnoughPlayers()` should return the expected output - a Boolean value - true or false
+* - Return true if the array size is atleast 7, otherwise false.
+*/
+// your code goes here
+const team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
+
+
+
 
 
 
